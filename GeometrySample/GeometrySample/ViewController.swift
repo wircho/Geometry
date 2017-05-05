5b00d1afc43af213bf89729a8d1b0efd69f7d369
AdolfoX Rodriguez <adolfox.rodriguez@intel.com>
2017-05-05 11:02:06 -0400

    Added Xcode project

---
+++ GeometrySample/GeometrySample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeometrySample
+//
+//  Created by AdolfoX Rodriguez on 2017-05-05.
+//  Copyright © 2017 Trovy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
